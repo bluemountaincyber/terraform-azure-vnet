3a344f24251fc09da6415a2424186238c563455c
ryananicholson <ryananicholson@gmail.com>
2022-09-21 13:03:15 -0400

    Added subnet_names output

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
 output "subnet_cidrs" {
   value       = local.subnet_cidrs
-  description = "Subnet CIDR ranges"
+  description = "Subnet CIDR ranges."
 }
 
 output "vnet_name" {
   value = azurerm_virtual_network.vnet.name
-  description = "Name of the Azure vnet"
+  description = "Name of the Azure vnet."
+}
+
+output "subnet_names" {
+  value = azurerm_subnet.subnet[*].name
+  description = "Subnet names."
 }
